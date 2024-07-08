2adbb25dcdc6b6addca37070fa79cb11d6e77516
DEVshot0 <alexandremachadomelo@hotmail.com>
2024-07-08 18:17:44 -0300

    adding personal account screen

--- components/Conta.js
+++ components/Conta.js
@@ -1,12 +1,97 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Image, View, Text, StyleSheet, TouchableOpacity, Dimensions } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
+import { FontAwesome } from '@expo/vector-icons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const { width: windowWidth } = Dimensions.get('window');
+const nome = "João"; // Defina o nome aqui
 
 export default function Conta() {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [localUri, setLocalUri] = useState(null);
+  const [greeting, setGreeting] = useState('');
+
+  useEffect(() => {
+    loadImageFromStorage();
+    setGreeting(getGreeting());
+  }, []);
+
+  const getGreeting = () => {
+    const currentHour = new Date().getHours();
+    if (currentHour < 12) {
+      return `Bom dia, ${nome}`;
+    } else if (currentHour < 18) {
+      return `Boa tarde, ${nome}`;
+    } else {
+      return `Boa noite, ${nome}`;
+    }
+  };
+
+  const loadImageFromStorage = async () => {
+    try {
+      const savedUri = await AsyncStorage.getItem('savedImageUri');
+      if (savedUri) {
+        setLocalUri(savedUri);
+        setSelectedImage(savedUri);
+      }
+    } catch (error) {
+      console.log('Erro ao carregar a imagem do armazenamento:', error);
+    }
+  };
+
+  const pickImage = async () => {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (status !== 'granted') {
+      alert('Desculpe, precisamos de permissões de galeria para isso funcionar!');
+      return;
+    }
+
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+      saveImageLocally(result.assets[0].uri);
+    }
+  };
+
+  const saveImageLocally = async (uri) => {
+    const fileName = uri.split('/').pop();
+    const newUri = FileSystem.documentDirectory + fileName;
+
+    try {
+      await FileSystem.copyAsync({
+        from: uri,
+        to: newUri
+      });
+      setLocalUri(newUri);
+      await AsyncStorage.setItem('savedImageUri', newUri);
+      console.log('Imagem salva localmente em:', newUri);
+    } catch (error) {
+      console.log('Erro ao salvar a imagem:', error);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Conta</Text>
-      <StatusBar style="light" />
+      <TouchableOpacity style={styles.photoContainer} onPress={pickImage}>
+        {selectedImage ? (
+          <Image source={{ uri: selectedImage }} style={styles.photo} />
+        ) : (
+          <View style={styles.photoPlaceholder}>
+            <FontAwesome name="camera" size={40} color="#fff" />
+          </View>
+        )}
+      </TouchableOpacity>
+      <Text style={styles.greetingText}>
+        {greeting.split(', ')[0]}, <Text style={styles.boldText}>{nome}.</Text>
+      </Text>
     </View>
   );
 }
@@ -16,7 +101,38 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#091015',
     alignItems: 'center',
+    paddingTop: 50, // Adiciona um espaçamento no topo
+  },
+  photoContainer: {
+    width: windowWidth * 0.6,
+    height: windowWidth * 0.6,
+    borderRadius: (windowWidth * 0.6) / 2,
+    borderWidth: 2,
+    borderColor: '#4682B4',
     justifyContent: 'center',
+    alignItems: 'center',
+    overflow: 'hidden',
+    marginTop: -10,
+  },
+  photo: {
+    width: '100%',
+    height: '100%',
+  },
+  photoPlaceholder: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#ccc',
+  },
+  greetingText: {
+    color: '#faffd6',
+    marginTop: 20,
+    textAlign: 'center',
+    fontSize: 24,
+  },
+  boldText: {
+    fontWeight: 'bold',
+    color: '#faffd6',
   },
 });
-
--- components/Veiculos.js
+++ components/Veiculos.js
@@ -1,21 +1,109 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, SafeAreaView, ScrollView, Alert, Dimensions } from 'react-native';
+import React, { useState } from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome'; // Import Icon from react-native-vector-icons
 
-export default function Veiculos() {
+const windowHeight = Dimensions.get('window').height;
+const windowWidth = Dimensions.get('window').width;
+
+const Veiculos = ({ navigation }) => {
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  // Array de opções para o menu
+  const options = ["Caminhão 1", "Caminhão 2", "Caminhão 3", "Caminhão 4", "Caminhão 5", "Caminhão 6", "Caminhão 7", "Caminhão 8", "Caminhão 9", "Caminhão 10","Caminhão 11","Caminhão 12","Caminhão 13","Caminhão 14","Caminhão 15","Caminhão 16","Caminhão 17"];
+
+  const handleOptionPress = (option) => {
+    setSelectedOption(option);
+  };
+
+  const handleChevronPress = () => {
+    if (!selectedOption) {
+      Alert.alert("Atenção", "Selecione uma das opções antes de prosseguir.");
+    } else {
+      console.log("direcionado para Cadastro.js");
+      navigation.navigate('Cadastro');
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Veiculos!</Text>
-      <StatusBar style="light" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        {options.map((option, index) => (
+          <TouchableOpacity
+            key={index}
+            onPress={() => handleOptionPress(option)}
+            style={[
+              styles.scrollItem,
+              selectedOption === option ? styles.selected : {}
+            ]}
+          >
+            <Text style={styles.scrollItemText}>{option}</Text>
+            <TouchableOpacity onPress={handleChevronPress}>
+              <Icon name="chevron-right" size={15} color="#4682B4" style={styles.rightIcon} />
+            </TouchableOpacity>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#091015',
-    alignItems: 'center',
+  },
+  content: {
+    flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: windowHeight * 0.12,
+    marginTop: windowHeight * 0.12,
+    backgroundColor: '#fff',
+    padding: 20,
+    width: windowWidth * 0.8,
+  },
+  logoContainer: {
+    width: '100%',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  scrollContainer: {
+    width: windowWidth * 0.7,
+    maxHeight: windowHeight * 0.3,
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  },
+  scrollView: {
+    width: '100%',
+  },
+  scrollItem: {
+    width: '100%',
+    fontSize: 16,
+    padding: 10,
+    borderBottomWidth: 0.7,
+    borderBottomColor: '#faffd6',
+    alignItems: 'center', // Align items to center to position text between the icons
+    flexDirection: 'row', // Add row direction to position icons and text in a row
+    justifyContent: 'space-between', // Space between elements to place rightIcon at the end
+  },
+  selected: {
+    borderColor: '#4682B4',
+    borderWidth: 5,
+    borderBottomWidth: 5,
+  },
+  scrollItemText: {
+    fontSize: 16.3,
+    color: '#faffd6',
+    flex: 1, // Add flex to occupy space between the icons
+    textAlign: 'left', // Align text to the left
+  },
+  rightIcon: {
+    marginLeft: 10,
   },
 });
+
+export default Veiculos;
